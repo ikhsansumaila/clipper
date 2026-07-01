0df3cbecbc7945ee75e7690f54056ab863fdd44e
ikhsansumaila <ikhsan.sumaila@gmail.com>
2026-07-01 14:05:51 +0800

    docs: add n8n workflow json file

--- scripts/discord-bridge.js
+++ scripts/discord-bridge.js
@@ -15,8 +15,16 @@ const clientStaging = new Client(options);
 const botToken = process.env.DISCORD_BOT_TOKEN;
 const botStagingToken = process.env.DISCORD_BOT_STAGING_TOKEN;
 
-async function handleMessage(env, message) {
+async function handleMessage(env, botUser, message) {
     if (message.author.bot) return;
+
+    // Hanya merespons jika bot ini yang di-mention
+    if (!message.mentions.has(botUser)) return;
+
+    // Pastikan ada link YouTube
+    if (!(message.content.includes('youtube.com') || message.content.includes('youtu.be'))) {
+        return;
+    }
 
     console.log(`[${env}] - Pesan diterima dari ${message.author.username}: ${message.content}`);
 
@@ -26,8 +34,6 @@ async function handleMessage(env, message) {
     );
 
     // 2. Kirim ke n8n TANPA menunggu selesai
-    // if (message.mentions.has(client.user) && (message.content.includes('youtube.com') || message.content.includes('youtu.be'))) {
-
     const n8nEndpoint = env === 'production' ? process.env.N8N_WEBHOOK_URL : process.env.N8N_WEBHOOK_TEST_URL;
     try {
         console.log(`[${env}] - Mengirim data ke n8n URL: ${n8nEndpoint}`);
@@ -37,15 +43,13 @@ async function handleMessage(env, message) {
             channelId: message.channelId,
             replyMessageId: processingMsg.id
         });
-        // await message.react('📥'); // Beri tanda centang/emoji masuk antrean
     } catch (err) {
         console.error(`Gagal mengirim data ke Webhook n8n: ${n8nEndpoint} \n`, err.message);
     }
-    // }
 }
 
-client.on('messageCreate', (message) => handleMessage('production', message));
-clientStaging.on('messageCreate', (message) => handleMessage('staging', message));
+client.on('messageCreate', (message) => handleMessage('production', client.user, message));
+clientStaging.on('messageCreate', (message) => handleMessage('staging', clientStaging.user, message));
 
 client.login(botToken);
 clientStaging.login(botStagingToken);
